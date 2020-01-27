786c6810f6198530af477aad2a58bd2e7718dcad
sy592 <59641196+sy592@users.noreply.github.com>
2020-01-27 14:07:18 -0500

    Update groceries.py
    Final Version of Groceries Code

--- groceries.py
+++ groceries.py
@@ -1,6 +1,7 @@
 # groceries.py
 
 #from pprint import pprint
+
 products = [
     {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
     {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
@@ -24,7 +25,84 @@ products = [
     {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
 ] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
 
-print(products)
-# pprint(products)
+#
+#PRODUCTS (PART 1)
+#
 
-# TODO: write some Python code here to produce the desired outpu
+products_count = len(products)
+print("--------------")
+print("THERE ARE " + str(products_count) + " PRODUCTS:")
+print("--------------")
+
+def sort_by_name(any_product):
+    return any_product["name"]
+
+sorted_products = sorted(products,key=sort_by_name)
+
+for p in sorted_products:
+    price_usd = " (${0:.2F})".format(p["price"])
+    print(" + " + p["name"] + price_usd)
+ 
+#
+#DEPARTMENTS (Part 2)
+#
+
+department = []
+
+for p in products:
+    department.append(p["department"])
+
+unique_departments = list(set(department))
+department_count = len(unique_departments)
+
+print("--------------")
+print("THERE ARE " + str(department_count) + " DEPARTMENTS:")
+print("--------------")
+
+unique_departments.sort()
+
+for d in unique_departments:
+    matching_products = [p for p in products if p["department"] == d]
+    matching_products_count = len(matching_products)
+    if matching_products_count > 1:
+        label = "products"
+    else:
+        label = "product"
+    print(" + " + d.title() + " (" + str(matching_products_count) + " " + label + ")")
+
+#--------------
+#THERE ARE 20 PRODUCTS:
+#--------------
+# + All-Seasons Salt ($4.99)
+# + Chocolate Fudge Layer Cake ($18.50)
+# + Chocolate Sandwich Cookies ($3.50)
+# + Cut Russet Potatoes Steam N' Mash ($4.25)
+# + Dry Nose Oil ($21.99)
+# + Fresh Scent Dishwasher Cleaner ($4.99)
+# + Gluten Free Quinoa Three Cheese & Mushroom Blend ($3.99)
+# + Green Chile Anytime Sauce ($7.99)
+# + Light Strawberry Blueberry Yogurt ($6.50)
+# + Mint Chocolate Flavored Syrup ($4.50)
+# + Overnight Diapers Size 6 ($25.50)
+# + Peach Mango Juice ($1.99)
+# + Pizza For One Suprema Frozen Pizza ($12.50)
+# + Pomegranate Cranberry & Aloe Vera Enrich Drink ($4.25)
+# + Pure Coconut Water With Orange ($3.50)
+# + Rendered Duck Fat ($9.99)
+# + Robust Golden Unsweetened Oolong Tea ($2.49)
+# + Saline Nasal Mist ($16.00)
+# + Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce ($6.99)
+# + Sparkling Orange Juice & Prickly Pear Beverage ($2.99)
+#--------------
+#THERE ARE 10 DEPARTMENTS:
+#--------------
+# + Babies (1 product)
+# + Beverages (5 products)
+# + Dairy Eggs (1 product)
+# + Dry Goods Pasta (1 product)
+# + Frozen (4 products)
+# + Household (1 product)
+# + Meat Seafood (1 product)
+# + Pantry (2 products)
+# + Personal Care (2 products)
+# + Snacks (2 products)
